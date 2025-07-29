aaeae269aca07ac7ab48822b2860155edc1a6c56
kjoon418 <kjoon418@naver.com>
2025-07-29 12:28:12 +0900

    [feat/#57] 상품 조회 API 구현

--- src/main/java/goodspace/backend/client/service/ClientServiceImpl.java
+++ src/main/java/goodspace/backend/client/service/ClientServiceImpl.java
@@ -3,7 +3,9 @@ package goodspace.backend.client.service;
 import goodspace.backend.client.dto.ClientBriefInfoResponseDto;
 import goodspace.backend.client.dto.ClientDetailsResponseDto;
 import goodspace.backend.client.domain.Client;
+import goodspace.backend.client.dto.ClientItemInfoResponseDto;
 import goodspace.backend.client.repository.ClientRepository;
+import goodspace.backend.global.domain.Item;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,8 @@ import java.util.function.Supplier;
 public class ClientServiceImpl implements ClientService {
     private static final Supplier<EntityNotFoundException> CLIENT_NOT_FOUND = () -> new EntityNotFoundException("클라이언트를 찾지 못했습니다.");
     private static final Supplier<IllegalStateException> PRIVATE_CLIENT = () -> new IllegalStateException("공개되지 않은 클라이언트입니다.");
+    private static final Supplier<EntityNotFoundException> ITEM_NOT_FOUND = () -> new EntityNotFoundException("상품을 찾지 못했습니다.");
+    private static final Supplier<IllegalStateException> PRIVATE_ITEM = () -> new IllegalStateException("공개되지 않은 상품입니다.");
 
     private final ClientRepository clientRepository;
 
@@ -24,9 +28,7 @@ public class ClientServiceImpl implements ClientService {
         Client client = clientRepository.findById(clientId)
                 .orElseThrow(CLIENT_NOT_FOUND);
 
-        if (!client.isPublic()) {
-            throw PRIVATE_CLIENT.get();
-        }
+        throwExceptionIfPrivate(client);
 
         return ClientDetailsResponseDto.of(client, true);
     }
@@ -39,5 +41,36 @@ public class ClientServiceImpl implements ClientService {
                 .filter(Client::isPublic)
                 .map(ClientBriefInfoResponseDto::from)
                 .toList();
+    }
+
+    @Override
+    public ClientItemInfoResponseDto getClientAndItem(long clientId, long itemId) {
+        Client client = clientRepository.findById(clientId)
+                .orElseThrow(CLIENT_NOT_FOUND);
+        Item item = findItemFromClient(client, itemId);
+
+        throwExceptionIfPrivate(client);
+        throwExceptionIfPrivate(item);
+
+        return ClientItemInfoResponseDto.of(client, item);
+    }
+
+    private Item findItemFromClient(Client client, long itemId) {
+        return client.getItems().stream()
+                .filter(item -> item.getId().equals(itemId))
+                .findAny()
+                .orElseThrow(ITEM_NOT_FOUND);
+    }
+
+    private void throwExceptionIfPrivate(Client client) {
+        if (!client.isPublic()) {
+            throw PRIVATE_CLIENT.get();
+        }
+    }
+
+    private void throwExceptionIfPrivate(Item item) {
+        if (!item.isPublic()) {
+            throw PRIVATE_ITEM.get();
+        }
     }
 }
