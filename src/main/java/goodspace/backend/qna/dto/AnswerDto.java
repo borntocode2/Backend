2c445a706013676954e968e591b0a54057c10421
ralph <sanghwaj222@gmail.com>
2025-08-05 13:30:49 +0900

    add anotation 'Getter' in AnswerDto

--- src/main/java/goodspace/backend/qna/dto/AnswerDto.java
+++ src/main/java/goodspace/backend/qna/dto/AnswerDto.java
@@ -1,10 +1,12 @@
 package goodspace.backend.qna.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Getter
 public class AnswerDto {
     private String content;
     private LocalDateTime createdAt;
