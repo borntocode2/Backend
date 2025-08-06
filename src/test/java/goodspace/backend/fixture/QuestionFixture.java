e07ae16b8813621ae39535b3768703595303f015
ralph <sanghwaj222@gmail.com>
2025-08-06 16:05:09 +0900

    [feat/#136] testcode for qna method

--- src/test/java/goodspace/backend/fixture/QuestionFixture.java
+++ src/test/java/goodspace/backend/fixture/QuestionFixture.java
@@ -40,11 +40,7 @@ public enum QuestionFixture {
     private final String content;
     private final QuestionType type;
 
-    QuestionFixture(
-            String title,
-            String content,
-            QuestionType type
-    ) {
+    QuestionFixture(String title, String content, QuestionType type) {
         this.title = title;
         this.content = content;
         this.type = type;
